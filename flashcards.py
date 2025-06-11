1cc63b2175300a37b3d9e822e58631c7d75f6ec6
mblinn <NovusTiro@gmail.com>
2025-06-10 21:51:07 -0400

    Add new flashcards and update layout

--- flashcards.py
+++ flashcards.py
@@ -12,4 +12,9 @@ flashcards = [
     Flashcard(front="La Maison", back="House"),
     Flashcard(front="Le Homme", back="Man"),
     Flashcard(front="L'Amour", back="Love"),
+    Flashcard(front="Savoir", back="To Know"),
+    Flashcard(front="Parler", back="To Talk"),
+    Flashcard(front="L'Ordinateur", back="Computer"),
+    Flashcard(front="La Norriture", back="Food"),
+    Flashcard(front="Le Verre", back="Glass"),
 ]
